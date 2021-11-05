b6e69066b69e2e6da5e41602e77c9a219e997b36
Fe <82155803+FeHeap@users.noreply.github.com>
2021-11-06 02:01:27 +0800

    NULL initial list

--- NCHU-CSE-DataStructure/Homework_2/ListOperation/ListOperation.c
+++ NCHU-CSE-DataStructure/Homework_2/ListOperation/ListOperation.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
 #define FreeLinkedList(list) freeLinkedList(&list)
@@ -120,6 +121,7 @@ enum readListData {
 };
 
 void constructLinkedList(LinkedList *list, char *listData) {
+	if(strlen(listData) == 0) { return; }
 	enum readListData state;
 	int i = 0;
 	while(TRUE) {
