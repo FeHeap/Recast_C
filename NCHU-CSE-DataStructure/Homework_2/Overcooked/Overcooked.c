265a7e96f6a8938f5fae1c6e480be56041851b83
Fe <82155803+FeHeap@users.noreply.github.com>
2021-11-16 05:26:15 +0800

    compare with value per time with time pass

--- NCHU-CSE-DataStructure/Homework_2/Overcooked/Overcooked.c
+++ NCHU-CSE-DataStructure/Homework_2/Overcooked/Overcooked.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
-#define RECIPES_FIN "test_case_2-1/recipes.txt" 
-#define ORDERS_FIN "test_case_2-1/orders.txt" 
+#define RECIPES_FIN "test_case_2-3/recipes.txt" 
+#define ORDERS_FIN "test_case_2-3/orders.txt" 
 #define PLAYER_FOUT "players.txt"
 #define TRUE 1
 
@@ -30,14 +31,17 @@ typedef struct vegetable {
 } Vegetable;
 
 typedef struct orderContent {
-	int DoOrNot;
+	int Done;
+	int CanOutTime;
+	int everDo;
+	
 	int number;
 	int cookNum;
 	Vegetable *cook;
 	int cutNum;
 	Vegetable *cut;
 	int otherTime;
-	int monny;
+	float monny;
 	int receiveTime;
 	int finishTime;
 } OrderContent;
@@ -61,15 +65,15 @@ Player p1, p2;
 struct processRecord {
 	int cookTime;
 	int cutTime;
-	int num;
-	int notToDo[200];
+	int BigStartIndex;
+	int BigEndIndex;
 } ProcessRecord;
 
-CookingTime* readRecipes();
+recipes* readRecipes();
 OrderContent* readOrder();
 void freeOrder(OrderContent**);
-void initialPlayer(Player*);
-void resetPlayer(Player*);
+void initialPlayer(Player*, int);
+void resetPlayer(Player*, int);
 void initialProcessRecord(struct processRecord*);
 void resetProcessRecord(struct processRecord*);
 void orderOrder(OrderContent*, int);
@@ -92,25 +96,235 @@ int main() {
 	}
 	
 	Recipes = readRecipes();
+	Order = NULL;
+	
+	while(TRUE) {
+		OrderContent *tempOrder = Order;
+		Order = readOrder();
+		orderOrder(Order, numOfOrder);
 
-	initialPlayer(&p1);
-	initialPlayer(&p2);
-	initialProcessRecord(&ProcessRecord);
-	Order = readOrder();
-	orderOrder(Order, numOfOrder);
-	int i, j;
-	int maxDeadline = 0;
-	for(i = 0; i < numOfOrder; i++) {
-		if(Order[i].finishTime > maxDeadline) {
-			maxDeadline = Order[i].finishTime;
+		int i, j;
+
+		if(tempOrder) {
+			for(i = 0; i < numOfOrder; i++) {
+				if(tempOrder[i].everDo == 1 && tempOrder[i].Done == 0 || tempOrder[i].everDo == 0 && tempOrder[i].Done == 1) {
+					Order[i].Done = 1;
+				}
+			}
+			freeOrder(&tempOrder);
+		}
+		else {
+			initialPlayer(&p1, Order[0].receiveTime);
+			initialPlayer(&p2, Order[0].receiveTime);
+			initialProcessRecord(&ProcessRecord);
+		}
+		
+		int maxDeadline = 0;
+		for(i = 0; i < numOfOrder; i++) {
+			if(Order[i].Done != 1 && Order[i].finishTime > maxDeadline) {
+				maxDeadline = Order[i].finishTime;
+			}
+		}
+		
+		int t;
+		for(t = Order[0].receiveTime; t < maxDeadline; /*pass*/) {
+			
+			for(i = ProcessRecord.BigStartIndex; i < numOfOrder; i++) {
+				if(t >= Order[i].receiveTime && t < Order[i].finishTime){
+					ProcessRecord.BigStartIndex = i;
+					break;
+				}
+			}
+			if(i == numOfOrder) {
+				break;
+			}
+			for(i = ProcessRecord.BigStartIndex; i < numOfOrder; i++) {
+				if(!(t >= Order[i].receiveTime && t < Order[i].finishTime)){
+					ProcessRecord.BigEndIndex = i;
+					break;
+				}
+			}
+			if(i == numOfOrder) {
+				ProcessRecord.BigEndIndex = numOfOrder;
+			}
+			
+			int equalFlag = (p1.time == p2.time);
+			Player *ptemp = (p1.time <= p2.time)? &p1 : &p2;
+			//printf("p1:%d p2:%d t:%d s:%d e:%d\n", p1.time, p2.time, t, ProcessRecord.BigStartIndex, ProcessRecord.BigEndIndex);
+			int NothingToDoFlag = 0;
+			int NothingToDo[20];
+			int NothingToDoIndex = 0;
+			while(TRUE) {
+				OrderContent *maxOrder = NULL;
+				for(i = ProcessRecord.BigStartIndex; i < ProcessRecord.BigEndIndex; i++) {
+					if(Order[i].Done == 0) {
+						int flag = 0;
+						for(j = 0; j < NothingToDoIndex; j++) {
+							if(Order[i].number == NothingToDo[j]) {
+								flag = 1;
+							}
+						}
+						if(flag) {
+							continue;
+						}
+						maxOrder = &Order[i];
+						break;
+					}
+				}
+				if(maxOrder == NULL) {
+					NothingToDoFlag = 1;
+					break;
+				}
+				for(i = i; i < ProcessRecord.BigEndIndex; i++) {
+					if(Order[i].Done == 0 && maxOrder->monny * maxOrder->monny / log(maxOrder->finishTime - t) < Order[i].monny * Order[i].monny / log(Order[i].finishTime - t)) {
+						int flag = 0;
+						for(j = 0; j < NothingToDoIndex; j++) {
+							if(Order[i].number == NothingToDo[j]) {
+								flag = 1;
+							}
+						}
+						if(flag) {
+							continue;
+						}
+						maxOrder = &Order[i];
+					}
+				}
+				
+				if(ptemp->time >= ProcessRecord.cookTime && maxOrder->cookNum != 0 && ptemp->time + 5 < maxOrder->finishTime) {
+					maxOrder->everDo = 1;
+					Process *temp = (Process*)malloc(sizeof(Process));
+					temp->number = maxOrder->number;
+					temp->processTime = ptemp->time;
+					ptemp->time += 5;
+					if(maxOrder->CanOutTime < ptemp->time) {
+						maxOrder->CanOutTime = ptemp->time;
+					}
+					ProcessRecord.cookTime = ptemp->time;
+					temp->type = 's';
+					strcpy(temp->vegetableToProcess, maxOrder->cook->cookWhat);
+					Vegetable* tempVegetable = maxOrder->cook;
+					maxOrder->cook = maxOrder->cook->next;
+					free(tempVegetable);
+					maxOrder->cookNum--;
+					temp->next = ptemp->Head;
+					ptemp->Head = temp;
+					break;
+				}
+				if(ptemp->time >= ProcessRecord.cutTime && maxOrder->cutNum != 0 && ptemp->time + 3 < maxOrder->finishTime) {
+					maxOrder->everDo = 1;
+					Process *temp = (Process*)malloc(sizeof(Process));
+					temp->number = maxOrder->number;
+					temp->processTime = ptemp->time;
+					ptemp->time += 3;
+					if(maxOrder->CanOutTime < ptemp->time) {
+						maxOrder->CanOutTime = ptemp->time;
+					}
+					ProcessRecord.cutTime = ptemp->time;
+					temp->type = 'c';
+					strcpy(temp->vegetableToProcess, maxOrder->cut->cookWhat);
+					Vegetable* tempVegetable = maxOrder->cut;
+					maxOrder->cut = maxOrder->cut->next;
+					free(tempVegetable);
+					maxOrder->cutNum--;
+					temp->next = ptemp->Head;
+					ptemp->Head = temp;
+					break;
+				}
+				if(maxOrder->cookNum == 0 && maxOrder->cutNum == 0 && ptemp->time >= maxOrder->CanOutTime && ptemp->time + maxOrder->otherTime <= maxOrder->finishTime) {
+					Process *temp = (Process*)malloc(sizeof(Process));
+					temp->number = maxOrder->number;
+					temp->processTime = ptemp->time;
+					ptemp->time += maxOrder->otherTime;
+					temp->type = 'f';
+					maxOrder->Done = 1;
+					temp->next = ptemp->Head;
+					ptemp->Head = temp;
+					break;
+				}	
+				NothingToDo[NothingToDoIndex++] = maxOrder->number;
+			}
+			
+			if(NothingToDoFlag) {
+				if(equalFlag) {
+					p1.time++;
+					p2.time++;
+					t++;
+				}
+				else {
+					ptemp->time++;
+					t++;
+				}
+			}
+			else {
+				t = (p1.time <= p2.time)? p1.time : p2.time;
+			}
+		}
+		
+		int doOverFlag = 0;
+		for(i = 0; i < numOfOrder; i++) {
+			if(Order[i].everDo == 1 && Order[i].Done == 0) {
+				doOverFlag = 1;
+			}
+		}
+		if(!doOverFlag) {
+			break;
+		}
+		resetPlayer(&p1, Order[0].receiveTime);
+		resetPlayer(&p2, Order[0].receiveTime);
+		resetProcessRecord(&ProcessRecord);
+	}
+	
+	Process *tempProcessHead = NULL;
+
+	while(p1.Head) {
+		Process *tempProcess = p1.Head;
+		p1.Head = p1.Head->next;
+		tempProcess->next = tempProcessHead;
+		tempProcessHead = tempProcess;
+	}
+	p1.Head = tempProcessHead;
+	
+	tempProcessHead = NULL;
+	while(p2.Head) {
+		Process *tempProcess = p2.Head;
+		p2.Head = p2.Head->next;
+		tempProcess->next = tempProcessHead;
+		tempProcessHead = tempProcess;
+	}
+	p2.Head = tempProcessHead;
+	while(p1.Head || p2.Head) {
+		if(p1.Head && p2.Head) {
+			if(p1.Head->processTime <= p2.Head->processTime) {
+				if(p1.Head->type != 'f')
+					printf("1 %d %d %c %s\n", p1.Head->processTime, p1.Head->number, p1.Head->type, p1.Head->vegetableToProcess);
+				else
+					printf("1 %d %d %c\n", p1.Head->processTime, p1.Head->number, p1.Head->type);
+				p1.Head = p1.Head->next;
+			}
+			else {
+				if(p2.Head->type != 'f')
+					printf("2 %d %d %c %s\n", p2.Head->processTime, p2.Head->number, p2.Head->type, p2.Head->vegetableToProcess);
+				else
+					printf("2 %d %d %c\n", p2.Head->processTime, p2.Head->number, p2.Head->type);
+				p2.Head = p2.Head->next;	
+			}
+		}
+		else if(p1.Head) {
+			if(p1.Head->type != 'f')
+				printf("1 %d %d %c %s\n", p1.Head->processTime, p1.Head->number, p1.Head->type, p1.Head->vegetableToProcess);
+			else
+				printf("1 %d %d %c\n", p1.Head->processTime, p1.Head->number, p1.Head->type);
+			p1.Head = p1.Head->next;
+		}
+		else {
+			if(p2.Head->type != 'f')
+				printf("2 %d %d %c %s\n", p2.Head->processTime, p2.Head->number, p2.Head->type, p2.Head->vegetableToProcess);
+			else
+				printf("2 %d %d %c\n", p2.Head->processTime, p2.Head->number, p2.Head->type);
+			p2.Head = p2.Head->next;
 		}
 	}
-	int t;
-	for(t = 0; t < maxDeadline; t++) {
-		
-	}
-	
-	
+
 	
 	// output file (players) opened with mode "w"
 	fout = fopen(PLAYER_FOUT, "w");
@@ -129,7 +343,7 @@ int main() {
 	return 0;
 }
 
-CookingTime* readRecipes() {
+recipes* readRecipes() {
 	// input file (recipes) opened with mode "r"
 	if((fin = fopen(RECIPES_FIN, "r")) == NULL) {
 		printf("Fail to open file %s!", RECIPES_FIN);
@@ -187,7 +401,8 @@ OrderContent* readOrder() {
 	OrderContent *order = (OrderContent*)malloc(numOfOrder * sizeof(OrderContent));
 	int i, j, k;
 	for(i = 0; i < numOfOrder; i++) {
-		order[i].DoOrNot = 0;
+		order[i].Done = 0;
+		order[i].everDo = 0;
 		fscanf(fin, "%d", &order[i].number);
 		charBuff = fgetc(fin);
 		order[i].cook = NULL;
@@ -217,6 +432,7 @@ OrderContent* readOrder() {
 		charBuff = fgetc(fin);
 
 		fscanf(fin, "%d", &order[i].receiveTime);
+		order[i].CanOutTime = order[i].receiveTime;
 		charBuff = fgetc(fin);
 		fscanf(fin, "%d", &order[i].finishTime);
 		charBuff = fgetc(fin);
@@ -225,7 +441,7 @@ OrderContent* readOrder() {
 		charBuff = fgetc(fin);
 		fscanf(fin, "%d", &Loss);
 		charBuff = fgetc(fin);
-		order[i].monny = Revenue - Loss;
+		order[i].monny = 1.0 * (Revenue - Loss) / (5 * order[i].cookNum + 3 * order[i].cutNum + order[i].otherTime);
 	}
 	
 	fclose(fin);
@@ -236,7 +452,7 @@ OrderContent* readOrder() {
 void freeOrder(OrderContent** order) {
 	int i;
 	for(i = 0; i < numOfOrder; i++) {
-		printf("%d\n", (*order+i)->number);
+		printf("%d %d %d %f\n", (*order+i)->number, (*order+i)->receiveTime, (*order+i)->finishTime, (*order+i)->monny);
 		Vegetable *tempVegetable = (*order+i)->cook;
 		while(tempVegetable != NULL) {
 			Vegetable *subTempVegetable = tempVegetable;
@@ -254,13 +470,13 @@ void freeOrder(OrderContent** order) {
 	*order = NULL;
 }
 
-void initialPlayer(Player *p) {
-	p->time = 0;
+void initialPlayer(Player *p, int startTime) {
+	p->time = startTime;
 	p->Head = NULL;
 }
 
-void resetPlayer(Player *p) {
-	p->time = 0;
+void resetPlayer(Player *p, int startTime) {
+	p->time = startTime;
 	while(p->Head) {
 		Process *temp = p->Head;
 		p->Head = p->Head->next;
@@ -271,12 +487,15 @@ void resetPlayer(Player *p) {
 void initialProcessRecord(struct processRecord* ProcessRecord) {
 	ProcessRecord->cookTime = 0;
 	ProcessRecord->cutTime = 0;
-	ProcessRecord->num = 0;
+	ProcessRecord->BigStartIndex = 0;
+	ProcessRecord->BigEndIndex = 1;
 }
 
 void resetProcessRecord(struct processRecord* ProcessRecord) {
 	ProcessRecord->cookTime = 0;
 	ProcessRecord->cutTime = 0;
+	ProcessRecord->BigStartIndex = 0;
+	ProcessRecord->BigEndIndex = 1;
 }
 
 void orderOrder(OrderContent *orders, int num) {
@@ -284,7 +503,7 @@ void orderOrder(OrderContent *orders, int num) {
 	for(i = 0; i < num-1; i++) {
 		swapIndex = i;
 		for(j = i+1; j < num; j++) {
-			if(orders[j].receiveTime < orders[swapIndex].receiveTime) {
+			if(orders[j].finishTime < orders[swapIndex].finishTime) {
 				swapIndex = j;
 			}
 		}
@@ -293,5 +512,15 @@ void orderOrder(OrderContent *orders, int num) {
 			orders[i] = orders[swapIndex];
 			orders[swapIndex] = temp;
 		} 
+	}
+	for(i = 0; i < num-2; i++) {
+		swapIndex = i;
+		for(j = i; j < num-1; j++) {
+			if(orders[j].receiveTime > orders[j+1].receiveTime) {
+				OrderContent temp = orders[j];
+				orders[j] = orders[j+1];
+				orders[j+1] = temp;
+			}
+		}
 	}
 }
