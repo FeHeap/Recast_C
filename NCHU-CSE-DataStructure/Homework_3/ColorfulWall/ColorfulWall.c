1e276ebc9cd35c89c62e60f0cded25385ca632e4
Fe <82155803+FeHeap@users.noreply.github.com>
2021-12-02 16:00:12 +0800

    BM

--- NCHU-CSE-DataStructure/Homework_3/ColorfulWall/ColorfulWall.c
+++ NCHU-CSE-DataStructure/Homework_3/ColorfulWall/ColorfulWall.c
@@ -1,204 +1,173 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define INPUT_FILE "test_case_2-3/input_2.txt" 
+#include <math.h>
+#include <ctype.h>
+#define INPUT_FILE "test_case_3-1/input_3.txt"
+#define OUTPUT_FILE "output_3.txt"
 #define TRUE 1
-#define NumOfCharType 128
 
-FILE *fin;
-
-typedef struct charType {
-	char whichChar;
-	struct charType *next;
-} CharType;
+FILE *fin, *fout;
 
 
-typedef struct heapUnit {
-	int sumOfNum;
-	CharType *types;
-} HeapUnit;
 
-typedef struct bitTable {
-	int *table;
-	struct bitTable *next;
-} BitTable;
+typedef struct block {
+	char *containToPrint;
+	char *colors;
+} Block;
+
+typedef struct blocksManagement {
+	int wallLength;
+	int numOfBlocks;
+	Block *blocks;
+} BlocksManagement;
+
+void instructionProcess(Block*, int, char*);
 
 int main() {
 	
+	// detect whether OUTPUT_FILE has already existed
+	if(!((fout = fopen(OUTPUT_FILE, "r")) == NULL)) {
+		fclose(fin);
+		printf("File %s has already existed! Overwrite?[Y/n] ", OUTPUT_FILE);
+		char Overwrite[10];
+		gets(Overwrite);
+		int i = 0;
+		while(isspace(Overwrite[i])) {
+			i++;
+		}
+		if(!(Overwrite[i] == 'y' || Overwrite[i] == 'Y')) {
+			exit(EXIT_SUCCESS);
+		}
+	}
 	// input file opened with mode "r"
 	if((fin = fopen(INPUT_FILE, "r")) == NULL) {
 		printf("Fail to open file %s!", INPUT_FILE);
 		exit(EXIT_FAILURE);
 	}
-
-	printf("Processing data...\n");
 	
-	printf("Outcome:\n");
+	// output file opened with mode "w"
+	fout = fopen(OUTPUT_FILE, "w");
 	
-	int numOfLines;
+	
+	
+	int wallLength;
+	int numOfInst;
 	char charBuff;
-	while(TRUE) {
-		fscanf(fin, "%d", &numOfLines);
-		charBuff = fgetc(fin);
-		if(numOfLines == 0) {
-			break;
-		}
-		
-		int i;
-		int *charOccurNum = (int*)calloc(NumOfCharType, sizeof(int));
-
-		for(i = 0; i < numOfLines; i++) {
-			while((charBuff = fgetc(fin)) != '\n') {
-				charOccurNum[charBuff] += 1;
-			}
-		}
-		
-		int numOfOccurCharType = 1;	// the index 0 of heap
-		for(i = 0; i < NumOfCharType; i++) {
-			if(charOccurNum[i] > 0) {
-				numOfOccurCharType++;
-			}
-		}
-		
-		HeapUnit *charOccurHeap = (HeapUnit*)malloc(numOfOccurCharType * sizeof(HeapUnit));
-		int top = 1;
-		for(i = 0; i < NumOfCharType; i++) {
-			if(charOccurNum[i] > 0) {
-				charOccurHeap[top].sumOfNum = charOccurNum[i];
-				charOccurHeap[top].types = (CharType*)malloc(sizeof(CharType));
-				charOccurHeap[top].types->whichChar = (char)i;
-				charOccurHeap[top++].types->next = NULL;
-				
-				int point = top - 1;
-				while(point > 1) {
-					if(charOccurHeap[point].sumOfNum < charOccurHeap[point/2].sumOfNum) {
-						HeapUnit temp = charOccurHeap[point];
-						charOccurHeap[point] = charOccurHeap[point/2];
-						charOccurHeap[point/2] = temp;
-						point /= 2;
-					}
-					else {
-						break;
-					}
-				}
-			}
-		}
-		
-		int *bitNumOfOccurChar = (int*)calloc(NumOfCharType, sizeof(int));
-		
-		while(top != 2) {
-			HeapUnit minSet_1 = charOccurHeap[1];
-			charOccurHeap[1] = charOccurHeap[--top];
-			int point = 1;
-			while(TRUE) {
-				if(point * 2 + 1 < top) {
-					if(charOccurHeap[point].sumOfNum > charOccurHeap[point * 2].sumOfNum || charOccurHeap[point].sumOfNum > charOccurHeap[point * 2 + 1].sumOfNum) {
-						HeapUnit *toChange = (charOccurHeap[point * 2].sumOfNum < charOccurHeap[point * 2 + 1].sumOfNum)? (charOccurHeap + point * 2) : (charOccurHeap + point * 2 + 1);
-						HeapUnit temp = *toChange;
-						*toChange = charOccurHeap[point];
-						charOccurHeap[point] = temp;
-						point = (int)(toChange - charOccurHeap);
-					}
-					else {
-						break;
-					}
-				}
-				else if(point * 2 < top) {
-					if(charOccurHeap[point].sumOfNum > charOccurHeap[point * 2].sumOfNum) {
-						HeapUnit temp = charOccurHeap[point * 2];
-						charOccurHeap[point * 2] = charOccurHeap[point];
-						charOccurHeap[point] = temp;
-						point *= 2;
-					}
-					else {
-						break;
-					}
-				}
-				else {
-					break;
-				}
-			}
-			
-			HeapUnit minSet_2 = charOccurHeap[1];
-			charOccurHeap[1] = charOccurHeap[--top];
-			point = 1;
-			while(TRUE) {
-				if(point * 2 + 1 < top) {
-					if(charOccurHeap[point].sumOfNum > charOccurHeap[point * 2].sumOfNum || charOccurHeap[point].sumOfNum > charOccurHeap[point * 2 + 1].sumOfNum) {
-						HeapUnit *toChange = (charOccurHeap[point * 2].sumOfNum < charOccurHeap[point * 2 + 1].sumOfNum)? (charOccurHeap + point * 2) : (charOccurHeap + point * 2 + 1);
-						HeapUnit temp = *toChange;
-						*toChange = charOccurHeap[point];
-						charOccurHeap[point] = temp;
-						point = (int)(toChange - charOccurHeap);
-					}
-					else {
-						break;
-					}
-				}
-				else if(point * 2 < top) {
-					if(charOccurHeap[point].sumOfNum > charOccurHeap[point * 2].sumOfNum) {
-						HeapUnit temp = charOccurHeap[point * 2];
-						charOccurHeap[point * 2] = charOccurHeap[point];
-						charOccurHeap[point] = temp;
-						point *= 2;
-					}
-					else {
-						break;
-					}
-				}
-				else {
-					break;
-				}
-			}
-			
-			minSet_1.sumOfNum += minSet_2.sumOfNum;
-			CharType *pointCharType = minSet_1.types;
-			while(pointCharType->next != NULL) {
-				bitNumOfOccurChar[pointCharType->whichChar]++;
-				pointCharType = pointCharType->next;
-			}
-			bitNumOfOccurChar[pointCharType->whichChar]++;
-			pointCharType->next = minSet_2.types;
-			pointCharType = pointCharType->next;
-			while(pointCharType != NULL) {
-				bitNumOfOccurChar[pointCharType->whichChar]++;
-				pointCharType = pointCharType->next;
-			}
-			
-			charOccurHeap[top++] = minSet_1;
-			point = top - 1;
-			while(point > 1) {
-				if(charOccurHeap[point].sumOfNum < charOccurHeap[point/2].sumOfNum) {
-					HeapUnit temp = charOccurHeap[point];
-					charOccurHeap[point] = charOccurHeap[point/2];
-					charOccurHeap[point/2] = temp;
-					point /= 2;
-				}
-				else {
-					break;
-				}
-			}
-		}
-		
-		int countBits = 0;
-		for(i = 0; i < NumOfCharType; i++) {
-			countBits += (bitNumOfOccurChar[i] * charOccurNum[i]);
-		}
-		printf("%d\n", countBits);
-		
-		free(charOccurNum);
-		free(bitNumOfOccurChar);
-		CharType *pointCharType = charOccurHeap[1].types;
-		while(pointCharType != NULL) {
-			CharType *temp = pointCharType;
-			pointCharType = pointCharType->next;
-			free(temp);
-		}
-		free(charOccurHeap);
+	fscanf(fin, "%d", &wallLength);
+	charBuff = fgetc(fin);
+	int numOfBlocks = (sqrt(wallLength) > (int)sqrt(wallLength))? ((int)sqrt(wallLength) + 1) : (int)sqrt(wallLength);
+	Block *blocks = (Block*)malloc(numOfBlocks * sizeof(Block));
+	int i;
+	for(i = 0; i < numOfBlocks; i++) {
+		blocks[i].containToPrint = (char*)malloc(2 * numOfBlocks * sizeof(char));
+		blocks[i].colors = (int*)malloc(numOfBlocks * sizeof(char));
 	}
 	
-	// close file
-	fclose(fin);
+	for(i = 0; i < numOfBlocks; i++) {
+		if(i * numOfBlocks < wallLength) {
+			blocks[i].containToPrint[0] = '_';
+			blocks[i].containToPrint[1] = '\0';
+			blocks[i].colors[0] = '\0';
+		}
+		else {
+			blocks[i].containToPrint[0] = '\0';
+		}
+	}
 	
-	system("PAUSE");
+	BlocksManagement BM;
+	BM.wallLength = wallLength;
+	BM.numOfBlocks = numOfBlocks;
+	BM.blocks = blocks;
+	
+	fscanf(fin, "%d", &numOfInst);
+	charBuff = fgetc(fin);
+	
+	char instBuff[40];
+	for(i = 0; i < numOfInst; i++) {
+		fscanf(fin, "%[^\n]", instBuff);
+		charBuff = fgetc(fin);
+	}
+	
+	
+	for(i = 0; i < numOfBlocks; i++) {
+		free(blocks[i].containToPrint);
+		free(blocks[i].colors);
+	}
+	free(blocks);
+	blocks = NULL;
+	BM.blocks = NULL;
+	
+	// close files
+	fclose(fin);
+	fclose(fout);
+	
 	return 0;
 }
+
+void query(BlocksManagement*, char*);
+void paint(BlocksManagement*, char*);
+
+void instructionProcess(BlocksManagement *BM, char *instruction) {
+	int  i = 0;
+	while(!isspace(instruction[i++]));
+	switch(instruction[0]) {
+		case 'Q':
+			//printf("Q\n");
+			query(BM, &instruction[i]);
+			break;
+		case 'P':
+			//printf("P\n");
+			paint(BM, &instruction[i]);
+			break;
+		default: // pass
+			break;
+	}
+}
+
+int strToInt(char *str) {
+	int returnInt = 0;	
+	int i;
+	for(i = 0; TRUE; i++) {
+		if(isspace(str[i]) || str[i] == '\0') {
+			break;
+		}
+		returnInt *= 10;
+		returnInt += (str[i] - '0');
+	}
+	return returnInt;
+}
+
+void query(BlocksManagement *BM, char *instruction) {
+	
+}
+
+void paint(BlocksManagement *BM, char *instruction) {
+	int left = strToInt(instruction);
+	int  i = 0;
+	while(!isspace(instruction[i++]));
+	int right = strToInt(instruction + i);
+	while(!isspace(instruction[i++]));
+	char color = instruction[i];
+	
+	int leftBlock = left / numOfBlocks;
+	int rightBlock = right / numOfBlocks;
+	
+	if(leftBlock == rightBlock) {
+		if(leftBlock == )
+		if(blocks[leftBlock].colors[0] == '\0') {
+			
+			for(i = 0; i < numOfBlocks; i++) {
+				
+			}
+		}
+		
+	}
+	
+	for(i = leftBlock + 1; i < rightBlock; i++) {
+		blocks[i].containToPrint[0] = color;
+		blocks[i].containToPrint[1] = '\0';
+		blocks[i].colors[0] = '\0';
+	}
+	
+}
+
